0756502bbd522a3906ede45a39a2c00c9eafd2fd
Wojt3kW <129041009+Wojt3kW@users.noreply.github.com>
2024-02-23 08:32:54 +0100

    load balancer on .NET with YARP

---
+++ Roles.Api/Controllers/RolesController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roles.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RolesController : ControllerBase
+    {
+        private readonly ILogger<RolesController> _logger;
+
+        public RolesController(ILogger<RolesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Role> Get()
+        {
+            var url = Request.GetEncodedUrl();
+            _logger.LogDebug($"get roles from '{url}'");
+
+            return Enumerable
+                .Range(1, 5)
+                .Select(index => new Role(index, $"Role{index}", $"{url}/{index}"))
+                .ToList();
+        }
+    }
+}
---
+++ Users.Api/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Users.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+         private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            var url = Request.GetEncodedUrl();
+            _logger.LogDebug($"get users from '{url}'");
+
+            return Enumerable
+                .Range(1, 5)
+                .Select(index => new User(index, $"User{index}", $"email{index}@domain.com", $"{url}/{index}"))
+                .ToList();
+        }
+    }
+}
